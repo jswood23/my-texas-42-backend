4d5fec2b1f87e331e3de513460593e00cf6fb870
jswood23 <56269953+jswood23@users.noreply.github.com>
2023-12-23 20:38:45 -0600

    update function for returning db info

--- data_access/database.go
+++ data_access/database.go
@@ -1,26 +1,47 @@
 package data_access
 
 import (
+	"database/sql"
 	"errors"
+	"fmt"
 	"os"
 )
 
-var dbAddress = ""
+var dbUrl = ""
+var dbPort = ""
+var dbName = ""
 var dbUsername = ""
 var dbPassword = ""
+var authSecretKey = ""
 
 func Initialize() error {
-	if len(os.Args) < 4 {
+	if len(os.Args) < 7 {
+		println("args:")
+		for _, arg := range os.Args {
+			println(arg)
+		}
+
 		return errors.New("not enough program arguments")
 	}
 
-	dbAddress = os.Args[1]
-	dbUsername = os.Args[2]
-	dbPassword = os.Args[3]
+	dbUrl = os.Args[1]
+	dbPort = os.Args[2]
+	dbName = os.Args[3]
+	dbUsername = os.Args[4]
+	dbPassword = os.Args[5]
+	authSecretKey = os.Args[6]
 
 	return nil
 }
 
-func GetDBLogin() (string, string, string) {
-	return dbAddress, dbUsername, dbPassword
+func GetDBSession() (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		dbUrl, dbPort, dbUsername, dbPassword, dbName)
+	db, err := sql.Open("postgres", psqlInfo)
+	return db, err
+}
+
+func GetAuthSecretKey() string {
+	return authSecretKey
 }
