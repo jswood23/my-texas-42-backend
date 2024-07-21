9980c04366861eba3907195315e7fbace91e74a2
jswood23 <56269953+jswood23@users.noreply.github.com>
2024-07-21 17:42:42 -0500

    add query function

--- services/database.go
+++ services/database.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	_ "github.com/lib/pq"
 	"my-texas-42-backend/system"
+	"reflect"
 )
 
 func GetDBSession() (*sql.DB, error) {
@@ -34,4 +35,37 @@ func CheckDBConnection() error {
 	}
 
 	return nil
+}
+
+// Query Note: T must be a struct with the same properties as the query returns in the same order
+func Query[T any](query string) ([]T, error) {
+	db, err := GetDBSession()
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	result, err := db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer result.Close()
+
+	var rows []T
+	for result.Next() {
+		var row T
+		val := reflect.ValueOf(&row).Elem()
+		var scanArgs []interface{}
+		for i := 0; i < val.NumField(); i++ {
+			scanArgs = append(scanArgs, val.Field(i).Addr().Interface())
+		}
+
+		err = result.Scan(scanArgs...)
+		if err != nil {
+			return nil, err
+		}
+		rows = append(rows, row)
+	}
+
+	return rows, nil
 }
